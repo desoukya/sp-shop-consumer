f1624cecd8867da46c705ea2904e365fb1b50b80
Amr Desouky <desoukya@gmail.com>
2022-12-13 15:31:50 -0600

    do not process bad messages

--- connectors/kafka.js
+++ connectors/kafka.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const { isEmpty } = require('lodash');
 const { Kafka } = require('kafkajs')
 const validate = require('../validation/kafka');
 const messages = require('../constants/messages');
@@ -24,28 +25,33 @@ const startKafkaConsumer = async () => {
   await consumer.subscribe({ topic, fromBeginning: true });
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
-      const parsedMessage = JSON.parse(message.value);
-      // determine which validation schema to use
-      const messageType = parsedMessage.meta.action;
-      // validate kafka message against schema prior to processing
-      const validatePayload = {
-        [messages.TICKET_PENDING]: validate.pendingTicketMessage,
-        [messages.TICKET_RESERVED]: validate.reservedTicketMessage,
-      }[messageType];
-      const validationError = validatePayload(parsedMessage);
-      if (validationError) {
-        return Promise.reject(validationError.message);
+      try {
+        // Deserialize message body
+        const parsedMessage = JSON.parse(message.value);
+        if (isEmpty(parsedMessage)) {
+          console.log('cannot process empty message')
+          return;
+        }
+
+        // process message if there is no validation error
+        const validationError = validate.kafkaMessage(parsedMessage);
+        if (!isEmpty(validationError)) {
+          console.log('cannot process message with validation error:', validationError.message)
+          return;
+        }
+
+        // determine which processor to call
+        const messageType = parsedMessage.meta.action;
+        const processMessage = {
+          [messages.TICKET_PENDING]: shopProcessor.processPendingTicket,
+          [messages.TICKET_RESERVED]: shopProcessor.processReservedTicket,
+        }[messageType];      
+
+        // call the processor
+        await processMessage(parsedMessage);
+      } catch (e) {
+        console.log('Unable to process message');
       }
-
-      // process message
-      const processMessage = {
-        [messages.TICKET_PENDING]: shopProcessor.processPendingTicket,
-        [messages.TICKET_RESERVED]: shopProcessor.processReservedTicket,
-      }[messageType];      
-      await processMessage(parsedMessage);
-
-      // successfully exit
-      return Promise.resolve();
     },
   });
 };
--- validation/kafka.js
+++ validation/kafka.js
@@ -1,36 +1,27 @@
 const Joi = require('@hapi/joi');
 const messages = require('../constants/messages');
-const { tickets } = require('./shared-schema');
 
 // pending/reserved message body
 const messageBody = Joi.object().keys({
   matchNumber: Joi.number().required(),
-  tickets,
+  tickets: Joi.array().items(
+    Joi.object().keys({
+      category: Joi.number().strict().valid(1, 2, 3).required(),
+      quantity: Joi.number().strict().min(1).max(2).required(),
+      price: Joi.number().strict().valid(75, 125, 195).required(),
+    }).required()
+  ).required().min(1).max(2),
 }).unknown(false);
 
-const reservationValidation = {
+const kafkaMessageValidation = {
   /**
-  * Validate schema for pending ticket
+  * Validate schema for pending/reserved ticket
   * @return null if validation passes otherwise a validation error
   */
-  pendingTicketMessage(reservation) {
+  kafkaMessage(reservation) {
     var schema = Joi.object().keys({
       meta: Joi.object().keys({
-        action: Joi.string().valid(messages.TICKET_PENDING).required(),
-      }).unknown(false),
-      body: messageBody,
-    }).required();
-    return schema.validate(reservation).error;
-  },
-
-  /**
-  * Validate schema for reserved ticket
-  * @return null if validation passes otherwise a validation error
-  */
-  reservedTicketMessage(reservation) {
-    var schema = Joi.object().keys({
-      meta: Joi.object().keys({
-        action: Joi.string().valid(messages.TICKET_RESERVED).required(),
+        action: Joi.string().valid(messages.TICKET_RESERVED, messages.TICKET_PENDING).required(),
       }).unknown(false),
       body: messageBody,
     }).required();
@@ -38,4 +29,4 @@ const reservationValidation = {
   },
 };
 
-module.exports = reservationValidation;
+module.exports = kafkaMessageValidation;
